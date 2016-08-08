75e1cc595f5e95803bd7adb95cd4eebf3ad2d455
Rodrigo Siqueira <knoxzin1@gmail.com>
2016-08-08 15:25:30 -0300

    Added fetchLocations tests

--- src/location/actions.test.js
+++ src/location/actions.test.js
@@ -1,7 +1,7 @@
 /*eslint-disable no-unused-vars*/
 import React from 'react';
 /*eslint-enable no-unused-vars*/
-
+import sinon from 'sinon';
 import { mockStore } from '../__mocks__/';
 import {
   FETCH_LOCATIONS_PENDING,
@@ -12,9 +12,69 @@ import {
 } from '../app/actionTypes';
 
 import { assert } from 'chai';
-import { openScanner, closeScanner } from './actions';
+import { fetchLocations, openScanner, closeScanner } from './actions';
+import * as firebase from '../app/firebase';
 
 describe('location/actions.js', () => {
+
+  it('calling fetchLocations should return a list of ids and locations', (done) => {
+    const expectedResult = {
+      1: {
+        name: 'Location 1',
+      },
+      2: {
+        name: 'Location 2',
+      },
+    };
+    const getLocations = sinon.stub(firebase, 'getLocations');
+    getLocations.returns(Promise.resolve(expectedResult));
+
+    const expectedActions = [
+      {
+        type: FETCH_LOCATIONS_PENDING,
+      },
+      {
+        type: FETCH_LOCATIONS_SUCCESS,
+        payload: {
+          ids: Object.keys(expectedResult),
+          locations: expectedResult,
+        },
+      },
+    ];
+
+    const store = mockStore({});
+    store.dispatch(fetchLocations())
+      .then(() => {;
+        assert.deepEqual(store.getActions(), expectedActions);
+        getLocations.restore();
+      })
+      .then(done);
+  });
+
+  it('should call FETCH_LOCATIONS_FAILURE if anything goes wrong', (done) => {
+    const expectedResult = new Error('something went wrong');
+    const getLocations = sinon.stub(firebase, 'getLocations');
+    getLocations.returns(Promise.reject(expectedResult));
+
+    const expectedActions = [
+      {
+        type: FETCH_LOCATIONS_PENDING,
+      },
+      {
+        type: FETCH_LOCATIONS_FAILURE,
+        payload: expectedResult,
+        error: true,
+      },
+    ];
+
+    const store = mockStore({});
+    store.dispatch(fetchLocations())
+      .then(() => {;
+        assert.deepEqual(store.getActions(), expectedActions);
+        getLocations.restore();
+      })
+      .then(done);
+  });
 
   it('should set scannerOpen to true after calling scannerOpen', () => {
     const expectedActions = [
